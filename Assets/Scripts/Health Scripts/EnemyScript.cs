3ecb27b1d4d828963db95205ac5f38f0497cda86
Jadervin <jdervin@ltu.edu>
2023-09-18 14:41:39 -0400

    Added enemy states

--- Assets/Scripts/Health Scripts/EnemyScript.cs
+++ Assets/Scripts/Health Scripts/EnemyScript.cs
@@ -2,18 +2,63 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyScript : EntityScript
+public enum EnemyStates
 {
+    IDLE,
+    PATROL,
+    CHASE,
+    ATTACK,
+    STUNNED,
+    DEATH
+}
+
+public enum AttackStates
+{
+    NON_COUNTERABLE,
+    COUNTERABLE
+}
+
+
+public abstract class  EnemyScript : EntityScript
+{
+
+    [SerializeField] private EnemyStates enemyState;
+    [SerializeField] private AttackStates attackState;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        enemyState = EnemyStates.IDLE;
+        attackState = AttackStates.NON_COUNTERABLE;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        switch(enemyState)
+        {
+            case EnemyStates.IDLE:
+                //Do nothing, unless I want the enemy to move as an idle state
+                break;
+            case EnemyStates.PATROL: 
+                //Movement
+                break;
+            case EnemyStates.CHASE:
+                //Track down and get into range of player
+                break;
+            case EnemyStates.ATTACK: 
+                //Executes attack and changes attack state to counterable
+                break;
+            case EnemyStates.STUNNED: 
+                //Enemy cannot move
+                break;
+            case EnemyStates.DEATH: 
+                //Enemy is dead and cannot do anything
+                break;
+            default: 
+                break;
+                    
+        }
     }
 
     public override void OnDeath()
