d261a501a46eea6e0de2efed0a07b643f3ab5e34
Jadervin <jdervin@ltu.edu>
2023-09-19 12:55:41 -0400

    Fixed enemyChase state

--- Assets/Scripts/Health Scripts/EnemyScript.cs
+++ Assets/Scripts/Health Scripts/EnemyScript.cs
@@ -21,33 +21,54 @@ public enum AttackStates
 
 public class EnemyScript : EntityScript
 {
-
     
     protected Rigidbody2D rb;
     protected BoxCollider2D boxCollider;
+
+    [Header("Components")]
+    [SerializeField] protected GameObject enemyParent;
+    [SerializeField] protected GameObject leftStopPoint;
+    [SerializeField] protected GameObject rightStopPoint;
+    [SerializeField] protected Transform playerTarget;
+
+
+    [Header("Collider Settings")]
+    //The length of the ground-checking collider
+    [SerializeField] protected float groundLength = 0.95f;
+
+    //The distance between the ground-checking colliders
+    [SerializeField] protected Vector3 colliderOffset;
+
+    [SerializeField] protected float chaseTriggerRadius = 3f;
+    [SerializeField] protected float attackTriggerRadius = 1.5f;
+    protected float oldChaseTriggerRadius;
+    protected float oldAttackTriggerRadius;
+
+
+    [Header("Movement Stats")]
     [SerializeField] protected float moveSpeed = 3f;
+    [SerializeField] protected float chaseSpeedMultiplier = 1.3f;
+
+    [Header("Booleans")]
     [SerializeField] protected bool isFacingRight;
-
-
-    
-
-    [SerializeField] private float chaseTriggerRadius = 3f;
+    [SerializeField] protected bool onEdgeOfGround;
     [SerializeField] protected bool canChase;
-    [SerializeField] private float attackTriggerRadius = 1.5f;
     [SerializeField] protected bool canAttack;
-    public LayerMask playerLayer;
-
-    [SerializeField] private Transform playerTarget;
-    //[SerializeField] private Collider2D playerCollider;
-    [SerializeField] private float chaseSpeedMultiplier = 1.3f;
 
     [Header("States")]
-    [SerializeField] private EnemyStates enemyState;
-    [SerializeField] private AttackStates attackState;
+    [SerializeField] protected EnemyStates enemyState;
+    [SerializeField] protected AttackStates attackState;
 
     [Header("Tags")]
     public string STOP_POINT_TAG = "StopPoint";
     public string CAMERA_SWITCH_TRIGGER_TAG = "CameraSwitchTriggers";
+    public string WALLTAG = "Wall";
+
+    [Header("Layer Masks")]
+    [SerializeField] protected LayerMask playerLayer;
+    [SerializeField] protected LayerMask groundLayer;
+
+
 
 
     // Start is called before the first frame update
@@ -57,13 +78,16 @@ public class EnemyScript : EntityScript
         attackState = AttackStates.NON_COUNTERABLE;
         rb = GetComponent<Rigidbody2D>();
         boxCollider = GetComponent<BoxCollider2D>();
+
+        oldChaseTriggerRadius = chaseTriggerRadius;
+        oldAttackTriggerRadius = attackTriggerRadius;
     }
 
     // Update is called once per frame
     void Update()
     {
         //Check if the player collides with any of the circle colliders made in this code
-        CheckCircleColliders();
+        CheckCustomColliders();
 
 
         switch (enemyState)
@@ -86,6 +110,7 @@ public class EnemyScript : EntityScript
 
             case EnemyStates.CHASE:
                 //Track down and get into range of player
+                //if()
                 ChasePlayer();
                 if (canAttack == true)
                 {
@@ -117,6 +142,7 @@ public class EnemyScript : EntityScript
 
     protected void Movement()
     {
+        //Moves in the direction the enemy is facing
         if (isFacingRightFunction())
         {
             rb.velocity = new Vector2(moveSpeed, 0f);
@@ -129,41 +155,99 @@ public class EnemyScript : EntityScript
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        //if it collides with one of the stop points while patrolling, rotate the enemy
         if(collision.gameObject.CompareTag(STOP_POINT_TAG) && enemyState == EnemyStates.PATROL)
         {
+            //if one of the colliders were turned off because it exited the chase state or reached the edge of the ground, turn that collider on after it collides with the other one.
+            if (collision.gameObject == rightStopPoint)
+            {
+                leftStopPoint.SetActive(true);
+                chaseTriggerRadius = oldChaseTriggerRadius;
+                attackTriggerRadius = oldAttackTriggerRadius;
+
+            }
+            else if(collision.gameObject == leftStopPoint)
+            {
+                rightStopPoint.SetActive(true);
+                chaseTriggerRadius = oldChaseTriggerRadius;
+                attackTriggerRadius = oldAttackTriggerRadius;
+            }
+            
             //Rotates the enemy when it reaches a stop point
             transform.localScale = new Vector2(-(Mathf.Sign(rb.velocity.x)), transform.localScale.y);
-            transform.localScale = new Vector2(-(Mathf.Sign(rb.velocity.x)), transform.localScale.y);
+            
         }
 
-        //if(collision.gameObject.CompareTag(CAMERA_SWITCH_TRIGGER_TAG))
-        //{
-        //    rb.velocity = new Vector2(0f, 0f);
-        //    enemyState = EnemyStates.IDLE;
-        //}
         
+
     }
 
+
+    //private void OnCollisionEnter2D(Collision2D collision)
+    // {
+    //    if (collision.gameObject.CompareTag(WALLTAG) && enemyState == EnemyStates.PATROL)
+    //    {
+
+
+    //        //Rotates the enemy when it reaches a stop point
+    //        transform.localScale = new Vector2(-(Mathf.Sign(rb.velocity.x)), transform.localScale.y);
+    //    }
+    //}
+
+
+    //Checks if the player is facing right
     protected bool isFacingRightFunction()
     {
         isFacingRight = transform.localScale.x > Mathf.Epsilon;
         return transform.localScale.x > Mathf.Epsilon;
     }
 
-    protected void CheckCircleColliders()
+    //Checks all of the custom colliders 
+    protected void CheckCustomColliders()
     {
         canChase = Physics2D.OverlapCircle(transform.position, chaseTriggerRadius, playerLayer);
 
         canAttack = Physics2D.OverlapCircle(transform.position, attackTriggerRadius, playerLayer);
 
+
+        //If the enemy is out of range of the chasing and attacking colliders while it is chasing or attack, set the state to patrol and turn off one of the stop points
         if(canChase == false && enemyState == EnemyStates.CHASE || canChase == false && enemyState == EnemyStates.ATTACK) 
         {
             enemyState = EnemyStates.PATROL;
 
+            TurnOffOneStopPoint();
+
+
+            //Rotates the enemy when it reaches a stop point
+            transform.localScale = new Vector2(-(Mathf.Sign(rb.velocity.x)), transform.localScale.y);
+        }
+
+        //Checks if the collider is not touching the ground
+        //if one of the raycast is not touching the ground, the boolean is set to true
+        onEdgeOfGround = !Physics2D.Raycast(transform.position + colliderOffset, Vector2.down, groundLength, groundLayer) || 
+            !Physics2D.Raycast(transform.position - colliderOffset, Vector2.down, groundLength, groundLayer);
+
+        //if the enemy is on the edge of the ground:
+            //set the chase to false
+            //turn off one of the stop points
+            //make the radiuses of the colliders zero
+            //change the enemyState to patrol
+        if (onEdgeOfGround)
+        {
+            canChase = false;
+            TurnOffOneStopPoint();
+            chaseTriggerRadius = 0;
+            attackTriggerRadius = 0;
+
+
+            //Rotates the enemy when it reaches a stop point
+            transform.localScale = new Vector2(-(Mathf.Sign(rb.velocity.x)), transform.localScale.y);
+            enemyState = EnemyStates.PATROL;
         }
     }
 
 
+    //Draws the colliders
     protected void OnDrawGizmosSelected()
     {
         //Drawing the Chase Trigger Circle
@@ -174,20 +258,25 @@ public class EnemyScript : EntityScript
         //Drawing the Attack Trigger Circle
         Gizmos.color = Color.red;
         Gizmos.DrawWireSphere(transform.position, attackTriggerRadius);
+
+        //if the player is on the edge of the ground, turn the color to yellow, else turn it to green
+        if (onEdgeOfGround)
+        {
+            Gizmos.color = Color.yellow;
+        }
+        else
+        {
+            Gizmos.color = Color.green;
+        }
+
+        Gizmos.DrawLine(transform.position + colliderOffset, transform.position + colliderOffset + Vector3.down * groundLength);
+        Gizmos.DrawLine(transform.position - colliderOffset, transform.position - colliderOffset + Vector3.down * groundLength);
     }
 
-
-    public override void OnDeath()
-    {
-        enemyState = EnemyStates.DEATH;
-        this.gameObject.SetActive(false);
-        //Destroy(gameObject);
-    }
-
-
-
+    //Chases the player
     protected void ChasePlayer()
     {
+        //Changes the direction the enemy based on which side the player is on
         if(transform.position.x < playerTarget.position.x)
         {
             rb.velocity = new Vector2(moveSpeed * chaseSpeedMultiplier, 0f);
@@ -201,5 +290,35 @@ public class EnemyScript : EntityScript
             isFacingRightFunction();
         }
     }
+
+    protected void TurnOffOneStopPoint()
+    {
+        //If the enemy is facing left and it's position is less than that of the leftStopPoint's position, turn off the leftStopPoint
+        if (transform.localScale.x == -1 && transform.position.x < leftStopPoint.transform.position.x)
+        {
+            leftStopPoint.SetActive(false);
+        }
+        //Else if the enemy is facing right and it's position is greater than that of the rightStopPoint's position, turn off the rightStopPoint
+        else if (transform.localScale.x == 1 && transform.position.x > rightStopPoint.transform.position.x)
+        {
+            rightStopPoint.SetActive(false);
+        }
+        //Else if the enemy is facing right and it's position is less than that of the rightStopPoint's position, turn off the leftStopPoint
+        else if (transform.localScale.x == 1 && transform.position.x < rightStopPoint.transform.position.x)
+        {
+            leftStopPoint.SetActive(false);
+        }
+    }
+
+    //This function overrides the one inheited by the entity script and when this script calls OnDamage and currentHealth = 0, this version of the function is called for the enemy.
+    public override void OnDeath()
+    {
+        enemyState = EnemyStates.DEATH;
+        enemyParent.gameObject.SetActive(false);
+        //Destroy(gameObject);
+    }
+
+
+
 }
 
