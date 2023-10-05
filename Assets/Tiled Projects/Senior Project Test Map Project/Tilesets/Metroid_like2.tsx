94583e737d2541b4f649cc7281881f3a3d8fba7c
Jadervin <jdervin@ltu.edu>
2023-10-04 22:14:33 -0400

    Imported Tiled World

--- Assets/Tiled Projects/Senior Project Test Map Project/Tilesets/Metroid_like2.tsx
+++ Assets/Tiled Projects/Senior Project Test Map Project/Tilesets/Metroid_like2.tsx
@@ -21,6 +21,25 @@
    <object id="1" x="0" y="0" width="16" height="16"/>
   </objectgroup>
  </tile>
+ <tile id="4">
+  <objectgroup draworder="index" id="2">
+   <object id="2" x="0" y="0" width="16" height="16" visible="0"/>
+   <object id="3" x="0" y="0" width="16" height="16" visible="0"/>
+   <object id="6" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="5">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="6">
+  <objectgroup draworder="index" id="2">
+   <object id="3" x="15.9386" y="0.903546">
+    <polygon points="0,0 -1.95166,0.0361419 -1.9878,1.04811 -4.04789,1.08426 -3.9756,2.09623 -4.98758,2.02394 -4.98758,3.07206 -5.92726,3.07206 -5.99955,5.05986 -6.93924,5.05986 -6.97538,9.90287 -6.97538,10.0836 -6.03569,10.0836 -5.99955,12.0714 -5.02372,12.1075 -4.98758,13.1195 -4.01175,13.1556 -4.01175,14.2399 -2.06009,14.1676 -1.9878,15.035 0.0361419,15.035"/>
+   </object>
+  </objectgroup>
+ </tile>
  <tile id="20">
   <objectgroup draworder="index" id="2">
    <object id="1" x="0" y="0" width="16" height="16"/>
