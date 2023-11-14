78d18ba293a2c95735e4c00b23e6dfe57435ea75
Jadervin <jdervin@ltu.edu>
2023-11-14 18:35:51 -0500

    Added Healthbar and Sound Volume Slider

--- Assets/Scripts/UI Scripts/PlayerHealthUI.cs
+++ Assets/Scripts/UI Scripts/PlayerHealthUI.cs
@@ -2,10 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class PlayerHealthUI : MonoBehaviour
 {
     [SerializeField] private PlayerHealth playerHealth;
+    [SerializeField] private Image barImage;
     [SerializeField] private TextMeshProUGUI currentHealthText;
     [SerializeField] private TextMeshProUGUI maxHealthText;
 
@@ -18,17 +20,22 @@ public class PlayerHealthUI : MonoBehaviour
 
         currentHealthText.text = playerHealth.GetCurrentHealth().ToString();
         maxHealthText.text = "/ " + playerHealth.GetMaxHealth().ToString();
+        barImage.fillAmount = (float)playerHealth.GetCurrentHealth() / playerHealth.GetMaxHealth();
     }
 
     private void PlayerHealth_OnPlayerMaxHealthChanged(object sender, System.EventArgs e)
     {
         maxHealthText.text = "/ " + playerHealth.GetMaxHealth().ToString();
         currentHealthText.text = playerHealth.GetCurrentHealth().ToString();
+
+        barImage.fillAmount = playerHealth.GetCurrentHealth() / playerHealth.GetMaxHealth();
     }
 
     private void PlayerHealth_OnPlayerHealthChanged(object sender, System.EventArgs e)
     {
         currentHealthText.text = playerHealth.GetCurrentHealth().ToString();
+
+        barImage.fillAmount = (float) playerHealth.GetCurrentHealth() / playerHealth.GetMaxHealth();
     }
 
     /*
