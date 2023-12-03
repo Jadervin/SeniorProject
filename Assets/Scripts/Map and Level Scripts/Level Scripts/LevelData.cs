3f288ba336fdf2706cd8788cf18bab871de98f7e
Jadervin <jdervin@ltu.edu>
2023-12-03 17:05:02 -0500

    Got Enemy Detection working

--- Assets/Scripts/Map and Level Scripts/Level Scripts/LevelData.cs
+++ Assets/Scripts/Map and Level Scripts/Level Scripts/LevelData.cs
@@ -5,9 +5,15 @@ using UnityEngine;
 public class LevelData : MonoBehaviour
 {
     public List<GameObject> colliderList = new List<GameObject>();
-    public LayerMask EnemyLayerMask;
+    private Collider2D[] hitColliders2D;
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    public LayerMask EnemyLayerMask;
+    public BoxCollider2D boxCollider2D;
+
+    public bool GotEnemies;
+    public Transform origin;
+
+    /*private void OnTriggerEnter2D(Collider2D collision)
     {
         if (!colliderList.Contains(collision.gameObject) && collision.gameObject.CompareTag(TagReferencesScript.ENEMYTAG))
         {
@@ -15,7 +21,7 @@ public class LevelData : MonoBehaviour
             Debug.Log("Added " + gameObject.name);
             Debug.Log("GameObjects in list: " + colliderList.Count);
         }
-    }
+    }*/
 
     /*private void OnCollisionEnter2D(Collision2D collision)
     {
@@ -50,7 +56,7 @@ public class LevelData : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        boxCollider2D = GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
@@ -61,7 +67,10 @@ public class LevelData : MonoBehaviour
 
     private void FixedUpdate()
     {
-        MyCollisions();
+        if (GotEnemies == false)
+        {
+            MyCollisions();
+        }
     }
 
 
@@ -69,16 +78,21 @@ public class LevelData : MonoBehaviour
     {
         //Use the OverlapBox to detect if there are any other colliders within this box area.
         //Use the GameObject's centre, half the size (as a radius) and rotation. This creates an invisible box around your GameObject.
-        Collider[] hitColliders = Physics.OverlapBox(gameObject.transform.position, transform.localScale / 2, Quaternion.identity, EnemyLayerMask);
-        int i = 0;
-        //Check when there is a new collider coming into contact with the box
-        while (i < hitColliders.Length)
+        hitColliders2D = Physics2D.OverlapBoxAll(origin.transform.position, boxCollider2D.size, 360.0f, EnemyLayerMask);
+
+        foreach (var hit in hitColliders2D)
         {
-            //Output all of the collider names
-            Debug.Log("Hit : " + hitColliders[i].name + i);
-            //Increase the number of Colliders in the array
-            i++;
+            if(hit.gameObject.TryGetComponent<EnemyScript>(out EnemyScript enemy))
+            {
+                if (enemy.GetEnemyID() != null)
+                {
+                    colliderList.Add(hit.transform.parent.gameObject);
+                }
+            }
         }
+
+        
+        GotEnemies = true;
     }
 
 
@@ -88,6 +102,7 @@ public class LevelData : MonoBehaviour
         Gizmos.color = Color.red;
         
         //Draw a cube where the OverlapBox is (positioned where your GameObject is as well as a size)
-        Gizmos.DrawWireCube(transform.position, transform.localScale);
+        Gizmos.DrawWireCube(origin.transform.position, boxCollider2D.size);
+        
     }
 }
