715924190c3dc12b2a35613fd74649c2fe988319
Jadervin <jdervin@ltu.edu>
2024-04-08 11:49:07 -0400

    Fixed Level Layout

---
+++ Assets/Tiled Projects/Senior Project Test Map Project/Worlds/Build 4 World/World Tilemaps/Section 1-1 Background.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Section 1-1 Background" tilewidth="480" tileheight="320" tilecount="1" columns="1">
+ <image source="../../../../../Class Files/College Classes/Senior (CLIMAX)/Senior Project/Sprites/Custom Sprites/Level Backgrounds/Section 1-1 Background.png" width="480" height="320"/>
+</tileset>
