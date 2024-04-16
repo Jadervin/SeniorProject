2a5a431fe59f6aa2193e8873138405583e20c963
Jadervin <jdervin@ltu.edu>
2024-04-16 10:32:30 -0400

    Got rid of debug notes

--- Assets/Scripts/Manager Scripts/SoundManager.cs
+++ Assets/Scripts/Manager Scripts/SoundManager.cs
@@ -211,29 +211,9 @@ public class SoundManager : MonoBehaviour
 
     private void PlaySound(AudioClip audioClip, Vector3 position, float volumeMultipler)
     {
-        if (audioClip == null)
-        {
-            Debug.LogError($"{nameof(audioClip)} is null on {gameObject.name}", this);
-            Debug.Log($"{nameof(audioClip)} is null on {gameObject.name}", this);
-        }
-
-        if (float.IsNaN(volumeMultipler))
-        {
-            Debug.LogError($"{nameof(volumeMultipler)} is null on {gameObject.name}", this);
-            Debug.Log($"{nameof(volumeMultipler)} is null on {gameObject.name}", this);
-        }
-
-        if (float.IsNaN(volume))
-        {
-            Debug.LogError($"{nameof(volume)} is null on {gameObject.name}", this);
-            Debug.Log($"{nameof(volume)} is null on {gameObject.name}", this);
-        }
-
         audioSource.PlayOneShot(audioClip, volumeMultipler * volume);
         //AudioSource.PlayClipAtPoint(audioClip, position, volumeMultipler * volume);
         
-
-
     }
 
     private void PlayFromArrayOfSounds(List<AudioClip> audioClipArray, Vector3 position)
